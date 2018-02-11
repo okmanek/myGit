3979b27cdfe0b027e26b85a38d9ccf366e3de236
okmanek <okmanek@onet.eu>
2018-02-11 14:44:19 +0100

    Parsing arguments fixed
    Right path to include Clara and changed main.cc

--- main.cc
+++ main.cc
@@ -1,48 +1,69 @@
 #include <iostream>
 #include <string>
-#include "my_math.hh"
+#include "lib/Clara/include/clara.hpp"
 
 #define print(x) std::cout << x << std::endl;
 #define println(x) std::cout << x << std::endl;
 
-//using namespace clara;
+using namespace clara;
+
+class CommandLineArguments
+{
+public:
+  int width = 0;
+  std::string name = "<your name>";
+  bool doIt = 0;
+};
 
 int main(int argc, char** argv)
 {
   std::cout << "File: main.cc" << std::endl;
-/*
+
+  CommandLineArguments myArgs;
+
+
   int width = 0;
   std::string name;
   bool doIt = false;
   std::string command;
 
+  // for an Opt instance like this:
+  //   auto a =
+  //   Opt( name, "name" )
+  //   ["-n"]["--name"]
+  //   ("the name to use")
+  // meaning of vars is:
+  //   name - bind variable / lambda
+  //   "name" - hint
+  //   "-n" / "--name" - option(s)
+  //   description(doc)
+
   auto cli
-    = Opt( width, "width" )
+    = Opt( myArgs.width, "width" )
            ["-w"]["--width"]
            ("How wide should it be?")
-    | Opt( name, "name" )
+    | Opt( myArgs.name, "name" )
            ["-n"]["--name"]
            ("By what name should I be known?")
-    | Opt( doIt )
+    | Opt( myArgs.doIt )
            ["-d"]["--doit"]
            ("Which command to run?");
 
   auto result = cli.parse( Args( argc, argv ) );
-*/
+
   // print(width); // should print arg given after "-w | --width"
   // print(name); // should print arg given after "-w | --width"
 
-  // test that includes work correctly
-  int x = factorial(3);
-  println("Factorial of 3 is: ");
-  print(x);
-
-  bool result = 0;
   if( !result )
   {
-    //std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
-    //exit(1);
+    std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+    exit(1);
   }
+
+  std::cout << "width: " << myArgs.width
+	        << ", name: "  << myArgs.name
+			<< ", doit: " << myArgs.doIt
+			<< std::endl;
 
   //print("text to print");
 }
